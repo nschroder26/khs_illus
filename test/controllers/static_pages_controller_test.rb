395f71b32a3834af3313f339de9361cd793d4677
Nicholas Schroder <nrs4492@hotmail.com>
2022-01-16 12:00:50 -0500

    add contact page. add tests to verify connection success to home, about, and contact pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,4 +10,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_about_url
     assert_response :success
   end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+  end
 end
