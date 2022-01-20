06ce0107f51c4738428fc03f496fc5400e9c8618
Nicholas Schroder <nrs4492@hotmail.com>
2022-01-19 19:58:59 -0500

    user login added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      reset_session
+      log_in user
+      redirect_to gallery_url
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,7 @@
 module SessionsHelper
+
+	#log in user
+	def log_in(user)
+		session[:user_id] = user.id
+	end
 end
