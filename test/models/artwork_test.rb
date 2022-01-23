11b3e97633ebb466d4973a5ab5fd55be8b0666ef
Nicholas Schroder <nrs4492@hotmail.com>
2022-01-23 12:49:47 -0500

    add valid test for artwork

--- test/models/artwork_test.rb
+++ test/models/artwork_test.rb
@@ -5,6 +5,10 @@ class ArtworkTest < ActiveSupport::TestCase
   def setup
     @artwork = Artwork.new(content: "Lorem ipsum", title: "Sample Title")
   end
+
+  test "should be valid" do
+    assert @artwork.valid?
+  end
   
   test "should be content" do
     @artwork.content = "     "
