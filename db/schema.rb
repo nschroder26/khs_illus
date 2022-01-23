a09cac52bde7b88eb4adff1750d4b96528ac5a44
Nicholas Schroder <nrs4492@hotmail.com>
2022-01-23 12:30:30 -0500

    add artwork model. add basic test to check for content and title

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_19_164026) do
+ActiveRecord::Schema.define(version: 2022_01_23_171754) do
+
+  create_table "artworks", force: :cascade do |t|
+    t.text "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
---
+++ test/models/artwork_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class ArtworkTest < ActiveSupport::TestCase
+
+  def setup
+    @artwork = Artwork.new(content: "Lorem ipsum", title: "Sample Title")
+  end
+  
+  test "should be content" do
+    @artwork.content = "     "
+    assert_not @artwork.valid?
+  end
+
+  test "should be title" do
+    @artwork.title = "    "
+    assert_not @artwork.valid?
+  end
+
+end
